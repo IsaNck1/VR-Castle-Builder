cdd866085947772e444604db2b8359a723f4cc8d
Isa <Isabell.Noack@stud.eah-jena.de>
2023-06-08 16:59:41 +0200

    info für bausystem

--- VR Castle Builder/Assets/Scripts/Perspektivenwechsel.cs
+++ VR Castle Builder/Assets/Scripts/Perspektivenwechsel.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Perspektivenwechsel : MonoBehaviour
 {
+	
+	// Baumeister.cs
+	/* public Perspektivenwechsel wechsel; // PW da reinziehen
+	update(){
+		...
+		if(!wechsel.innenAnsicht) bauen();
+		...
+	}*/
 
     public GameObject terrain;
 	public GameObject tisch;
 	public GameObject scene;
 	public GameObject player;
-	public bool innenAnsicht = true;
+	public bool innenAnsicht = true; // Nur bauen wenn innenAnsicht false
 
 	public float outerScale = 0.05f;
 	public float outerPositionY = 1.0f;
+	public float entfernungZumTisch = 1.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -35,7 +44,7 @@ public class Perspektivenwechsel : MonoBehaviour
 					scene.transform.localScale = new Vector3(1,1,1);
 				} else {
 					// Schrumpfe Burg und platziere sie vorm Spieler
-					scene.transform.localPosition = player.transform.position + player.transform.forward + new Vector3(0,outerPositionY,0);
+					scene.transform.localPosition = player.transform.position + entfernungZumTisch * player.transform.forward + new Vector3(0,outerPositionY,0);
 					scene.transform.localEulerAngles = new Vector3(0,player.transform.eulerAngles.y,0);
 					scene.transform.localScale = new Vector3(outerScale,outerScale,outerScale);
 				}
