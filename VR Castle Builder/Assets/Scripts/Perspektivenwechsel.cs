1cc3a34be0c697cd9dff76b7176b3e366f3f1584
Isa <Isabell.Noack@stud.eah-jena.de>
2023-06-08 16:49:16 +0200

    perspektivenwechsel transformation
    und tisch added

--- VR Castle Builder/Assets/Scripts/Perspektivenwechsel.cs
+++ VR Castle Builder/Assets/Scripts/Perspektivenwechsel.cs
@@ -6,6 +6,13 @@ public class Perspektivenwechsel : MonoBehaviour
 {
 
     public GameObject terrain;
+	public GameObject tisch;
+	public GameObject scene;
+	public GameObject player;
+	public bool innenAnsicht = true;
+
+	public float outerScale = 0.05f;
+	public float outerPositionY = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +25,21 @@ public class Perspektivenwechsel : MonoBehaviour
     {
         if (Input.GetKeyDown("k"))
         {
-            if (terrain != null) terrain.SetActive(!terrain.activeSelf);
+			innenAnsicht = !innenAnsicht;
+            if (terrain != null) terrain.SetActive(innenAnsicht);
+			if (tisch != null) tisch.SetActive(!innenAnsicht);
+			if (scene != null && player != null) {
+				if(innenAnsicht) {
+					// Teleportiere Burg an die Position des Spielers
+					scene.transform.localPosition = player.transform.position;
+					scene.transform.localScale = new Vector3(1,1,1);
+				} else {
+					// Schrumpfe Burg und platziere sie vorm Spieler
+					scene.transform.localPosition = player.transform.position + player.transform.forward + new Vector3(0,outerPositionY,0);
+					scene.transform.localEulerAngles = new Vector3(0,player.transform.eulerAngles.y,0);
+					scene.transform.localScale = new Vector3(outerScale,outerScale,outerScale);
+				}
+			}
         }
     }
 }
